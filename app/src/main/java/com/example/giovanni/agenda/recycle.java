a82f2c2f2cfd53cf8b66e67c37727e4053f1b6e0
gizattos <gizattos@hotmail.com>
2018-05-14 22:31:53 -0400

    Recycleview funcionando

--- app/src/main/java/com/example/giovanni/agenda/recycle.java
+++ app/src/main/java/com/example/giovanni/agenda/recycle.java
@@ -32,37 +32,25 @@ public class recycle extends AppCompatActivity {
 
     FirebaseDatabase database;
     FirebaseAuth auth;
-    String nome;
-    String tipo;
-    String nascimento;
-    String[] testando = new String[10];
-    String[] testando2 = new String[10];
-    String[] testando3 = new String[10];
-    int v=0;
-    int s=0;
-    int f=0;
-    int contador=0;
+    AlunoAdapter adapter;
 
-
-
-    public class Aluno{
+    public static class Aluno{
         public String tipo;
         public String nome;
-        public String nascimento;
+        public String dt_nasc;
 
-        public Aluno(String tipo,String nome,String nascimento){
+        public Aluno (){ }
+
+
+        public Aluno(String tipo,String nome,String dt_nasc){
             this.tipo = tipo;
             this.nome = nome;
-            this.nascimento = nascimento;
-
-
-
+            this.dt_nasc = dt_nasc;
         }
-
     }
 
-    private List<Aluno> listaAlunos = new ArrayList<>();
 
+    private List<Aluno> listaAlunos = new ArrayList<>();
 
 
     @Override
@@ -73,92 +61,30 @@ public class recycle extends AppCompatActivity {
         database = FirebaseDatabase.getInstance();
         auth = FirebaseAuth.getInstance();
 
-        FirebaseUser user = auth.getCurrentUser();
-     //   DatabaseReference alunos = database.getReference("/Alunos");
-        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
-        DatabaseReference alunos = rootRef.child("/Alunos");
-
-
-
-
-
+        DatabaseReference alunos = database.getReference("/Alunos");
 
         alunos.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot snapshot) {
-                System.out.println(snapshot.getValue());  //prints "Do you have data? You'll love Firebase."
-                nascimento = snapshot.getValue().toString();
                 for(DataSnapshot ds : snapshot.getChildren()) {
-                   testando3[f] = ds.child("dt_nasc").getValue(String.class);
-                    testando2[f] = ds.child("nome").getValue(String.class);
-                    testando[f] = ds.child("tipo").getValue(String.class);
-                    Log.d("TAG","o nome é" + testando3[f]);
-
-
-
-                    Aluno aluno1 = new Aluno(testando[f],testando2[f],testando3[f]);
-                    listaAlunos.add(aluno1);
-
-                    contador++;
-                    f++;
+                    Aluno aluno = ds.getValue(Aluno.class);
+                    listaAlunos.add(aluno);
                 }
-
-
-
-
-                //Aluno aluno1 = new Aluno(testando[0],testando2[0],testando3[0]);
-               // listaAlunos.add(aluno1);
-               // Aluno aluno2 = new Aluno(testando[1],testando2[1],testando3[1]);
-              //  listaAlunos.add(aluno2);
-             //   Aluno aluno3 = new Aluno(testando[2],testando2[2],testando3[2]);
-            //    listaAlunos.add(aluno3);
-           //     Aluno aluno4 = new Aluno(testando[3],testando2[3],testando3[3]);
-          //      listaAlunos.add(aluno4);
-
+                adapter.notifyDataSetChanged();
             }
+
             @Override
             public void onCancelled(DatabaseError databaseError) {
             }
         });
 
 
-
-        Collection allListObject = new ArrayList();
-        for (int i = 0; i < 10; i++) {
-            ArrayList<Integer> list = new ArrayList<Integer>();
-         /* operation with list */
-            list.add(i);
-            allListObject.add(list);
-        }
-
-        //iterate through the ArrayList values using Iterator's hasNext and next methods
-        for (Iterator it = allListObject.iterator(); it.hasNext();) {
-            System.out.println(it.next());
-        }
-
-
-        //Aluno aluno1 = new Aluno(tipo,nome,nascimento);
-      //  Aluno aluno1 = new Aluno(tipo,nome,nascimento);
-
-
-
-
-
-
-
-
-
-
         RecyclerView lista = findViewById(R.id.Lista);
         lista.setLayoutManager(new LinearLayoutManager(this));
         lista.setItemAnimator(new DefaultItemAnimator());
 
-
-        AlunoAdapter adapter = new AlunoAdapter(this);
+        adapter = new AlunoAdapter(this);
         lista.setAdapter(adapter);
-
-
-
     }
 
     public class AlunoViewHolder extends RecyclerView.ViewHolder{
@@ -171,9 +97,7 @@ public class recycle extends AppCompatActivity {
             nome = view.findViewById(R.id.nome);
             matricula = view.findViewById(R.id.matricula);
             curso = view.findViewById(R.id.curso);
-
         }
-
     }
 
 
@@ -197,7 +121,7 @@ public class recycle extends AppCompatActivity {
 
             holder.nome.setText(a.tipo);
             holder.matricula.setText(a.nome);
-            holder.curso.setText(a.nascimento);
+            holder.curso.setText(a.dt_nasc);
 
         }
 
@@ -208,6 +132,8 @@ public class recycle extends AppCompatActivity {
     }
 
 }
+
+
 
 
 // adicionar os campos nome, matricula e curso no xmllayoutfile, layout height tem que ser wrap content
